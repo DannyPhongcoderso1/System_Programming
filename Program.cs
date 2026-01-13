7403dd6e9c2e8dc08e374091ae9421dd8eb7883c
DannyPhongcoderso1 <dannyphong2407@gmail.com>
2026-01-13 11:48:37 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Tutorial_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("THÔNG TIN MÔI TRƯỜNG HỆ THỐNG");
+
+            string osVersion = Environment.OSVersion.ToString();
+
+            string currentDir = Environment.CurrentDirectory;
+
+            DateTime currentTime = DateTime.Now;
+
+            Console.WriteLine($"1. Phiên bản HĐH: {osVersion}");
+            Console.WriteLine($"2. Thư mục hiện tại: {currentDir}");
+            Console.WriteLine($"3. Thời gian hệ thống: {currentTime:dd/MM/yyyy HH:mm:ss}");
+            Console.ReadLine();
+        }
+    }
+}
